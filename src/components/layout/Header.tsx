336600b4692e19016cd8c306e9a9f8d424f057ce
Cliff263 <cliffjaure263@gmail.com>
2025-03-12 22:53:57 +0200

    update

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -62,10 +62,11 @@ const Header = () => {
               </button>
               <Link href='/auth/sign-in'>SignIn</Link>
               <Link href='/auth/sign-out'>SignUp</Link>
-              <button className='text-gray-700 hover:text-gray-900 relative' title="Search" type='button'>
+              <button className='text-gray-700 hover:text-gray-900 relative' title="" type='button'>
                 <svg xmlns='http://www.w3.org/2000/svg' className='h-5 w-5 sm:h-6 sm:w-6' fill='none' viewBox='0 0 24 24' stroke='currentColor'>
                   <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z' />
-                </svg>              
+                </svg>
+                <span className='absolute -top-1 -right-1 w-4 h-4 bg-red-500 text-white text-xs font-medium flex items-center justify-center rounded-full'>0</span>              
               </button>
             </div>
           </div>
