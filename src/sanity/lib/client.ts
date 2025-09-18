1fc5ad747cf85da4a89375294cddd5166ce3c101
Cliff263 <cliffjaure263@gmail.com>
2025-09-18 23:04:23 +0200

    client fix

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,14 +1,5 @@
-import { createClient } from 'next-sanity'
-import { apiVersion, dataset, projectId } from '../env'
 import { sanityFetch } from './live'
 import { Product, ProductCategory, PromotionCampaign, PromotionCode } from '@/sanity.types'
-
-export const client = createClient({
-  projectId,
-  dataset,
-  apiVersion,
-  useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
-})
 export const getAllProducts = async () => {
   const query = `*[_type == "product"] | order(_createdAt desc) [0...24] {
     _id,
--- src/sanity/lib/live.ts
+++ src/sanity/lib/live.ts
@@ -2,7 +2,7 @@
 // Before using it, import and render "<SanityLive />" in your layout, see
 // https://github.com/sanity-io/next-sanity#live-content-api for more information.
 import { defineLive } from "next-sanity";
-import { client } from './client'
+import { client } from './sanity-client'
 
 const token = process.env.SANITY_API_READ_TOKEN;
 if(!token) {
